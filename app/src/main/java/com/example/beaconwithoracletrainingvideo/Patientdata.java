fd03cfefd6ef579017cceab89c87da801824881e
FalconBFR <0220182218@harrowschool.hk>
2020-04-19 18:29:10 +0800

    Adding server reference and compare capability, IllCloseContacts Button to view confirmed cases of close contacts in dbview To be implimented: Do not rewrite ill close contacts if uuid exists already in txt Scroll View UUID auto Accounts for about 10 commits, forgot to commit

---
+++ app/src/main/java/com/example/beaconwithoracletrainingvideo/Patientdata.java
@@ -0,0 +1,40 @@
+package com.example.beaconwithoracletrainingvideo;
+
+public class Patientdata {
+    private String uuid;
+    private String situation;
+    private String date;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Patientdata String{" +
+                "uuid='" + uuid + '\'' +
+                ", situation='" + situation + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
