9089b5622df9acdf3ccf849a5ab824e3be56171b
FalconBFR <0220182218@harrowschool.hk>
2020-04-28 19:08:49 +0800

    /uuid/new works

---
+++ classuuid.py
@@ -0,0 +1,12 @@
+# class uuid():
+#     def __init__(self,info):
+#         '''
+#         :param info: a list containing : [situation,uuid,date]
+#         '''
+#         self.uuid()
+#     def getsituation(self):
+#         return self.situation
+#     def getuuid(self):
+#         return self.uuid
+#     def getdate(self):
+#         return self.date
