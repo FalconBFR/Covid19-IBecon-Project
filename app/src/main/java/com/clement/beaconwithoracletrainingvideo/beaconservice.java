b0beff26eea82cc300b3aa26ced62735ac4ba270
FalconBFR <0220182218@harrowschool.hk>
2020-04-29 18:31:44 +0800

    Added Notif Bar and made it a foreground service

--- app/src/main/java/com/clement/beaconwithoracletrainingvideo/beaconservice.java
+++ app/src/main/java/com/clement/beaconwithoracletrainingvideo/beaconservice.java
@@ -1,5 +1,9 @@
 package com.clement.beaconwithoracletrainingvideo;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -7,6 +11,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -19,6 +24,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.NotificationCompat;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -50,6 +56,10 @@ import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class beaconservice extends Service implements BeaconConsumer {
+
+    private static final String CHANNEL_ID = "Detecting Close Contacts" ;
+    private NotificationManager mNM;
+    private int NOTIFICATION = R.string.local_service_started;
 
     @Override
     public Context getApplicationContext() {
@@ -209,6 +219,11 @@ public class beaconservice extends Service implements BeaconConsumer {
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout((IBEACON_LAYOUT))); //SWITCH TO IBEACON LATER //Todo: switch to IBeacon
         beaconManager.bind(this);
+
+        //showing a notif
+        //mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Display a notification about us starting.  We put an icon in the status bar.
+        //showNotification(getString(R.string.servicerunningnote));
     }
 
     @Override
@@ -220,6 +235,20 @@ public class beaconservice extends Service implements BeaconConsumer {
         Message msg = serviceHandler.obtainMessage();
         msg.arg1 = startId;
         serviceHandler.sendMessage(msg);
+
+        //for the notification
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Detecting Close Contacts")
+                .setContentText("Clement's Beacon App Running as usual. Nothing to worry about")
+                .setSmallIcon(R.drawable.notifbar)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
 
         // If we get killed, after returning from here, restart
         return START_STICKY;
@@ -468,6 +497,42 @@ public class beaconservice extends Service implements BeaconConsumer {
             savinguuidtotxt(dataline);
 
             return patientsdata;
+        }
+    }
+
+    private void showNotification(String tobeshowntext){
+        // In this sample, we'll use the same text for the ticker and the expanded notification
+        CharSequence text = tobeshowntext;
+
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        // Set the info for the views that show in the notification panel.
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.notifbar)  // the status icon
+                .setTicker(text)  // the status text
+                .setWhen(System.currentTimeMillis())  // the time stamp
+                .setContentTitle(getText(R.string.servicerunning))  // the label of the entry
+                .setContentText(text)  // the contents of the entry
+                .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
+                .build();
+
+
+
+        // Send the notification.
+        mNM.notify(NOTIFICATION, notification);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
         }
     }
 }
